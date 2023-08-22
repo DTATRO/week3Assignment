9044381553d9701dd03b2b93fc60244b33683f9e
DTATRO <daniellemarietatro05@gmail.com>
2023-08-21 20:50:04 -0700

    script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("hello world")
+
+
+/**
+ * comparison operators
+ * 
+ * 5 > 10 // boolean
+ * 5 >= 10
+ * 5 < 10
+ * 5 <= 10
+ * 5 == 10
+ */
+
+// if (true) {
+//     //do the thing
+// } else {
+//     //top was false, do this one
+// }
+
+// while (true) {
+
+// }
+
+// do {
+
+// } while(true)
+
+for (let i = 10; i >= 0; i--) {
+    console.log(i)
+}
+
+function myFunction () {
+
+}
+
+//es6
+const myFunction = () => {
+    return 'Hello World'
+}
